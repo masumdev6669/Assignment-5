217fedf1fbc62baea5b5582a471d82878464b49d
Masum <mahbubmasum2019@gmail.com>
2026-09-12 18:01:43 +0600

    Navbar Completed

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Nav from "./components/Nav"
+function App() {
+
+
+  return (
+    <>
+       <Nav />
+
+
+
+        
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Nav.tsx
@@ -0,0 +1,29 @@
+import Logo from "../assets/logo-text.png"
+
+
+const Nav = () => {
+    return (
+         <nav className=" flex justify-between mt-5 container mx-auto">
+               <img src={Logo} alt="" />
+               <ul className="flex gap-5">
+                <li><a href="">Home</a></li>
+                <li><a href="">Technologies</a></li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+                
+                
+               </ul>
+
+               <div className="flex gap-4 items-center -mt-4">
+                   <button >Sign in</button>
+                   <button className="btn btn-secondary rounded-2xl text-white">Sign Up</button>
+                   
+               </div>
+        
+
+      </nav> 
+    );
+};
+
+export default Nav;
