38a441fe55cc68ac4b18e0ba4c8cdff78fb2c921
Masum <mahbubmasum2019@gmail.com>
2026-09-13 20:41:51 +0600

    Responsive Hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,29 +2,36 @@ import Logo from "../assets/banner-stack.png"
 
 const Hero = () => {
     return (
-
-         <div className="mt-30 container mx-auto flex justify-between">
-            <div className=" mx-0 my-auto space-y-6">
+         <div className="mt-10 md:mt-30 container mx-auto px-4 flex flex-col md:flex-row justify-between items-center gap-10">
+            <div className="w-full md:w-1/2 mx-auto text-center md:text-left space-y-6">
                 
-                     <h1 className="text-6xl font-bold items-center ">Build Your Ideal <br /> <span className= " bg-linear-to-l from-purple-700 to-[#F97316] rple-700 bg-clip-text text-transparent ">Development Stack</span></h1>
+                <h1 className="text-4xl md:text-6xl font-bold">
+                    Build Your Ideal <br /> 
+                    <span className="bg-linear-to-l from-purple-700 to-[#F97316] bg-clip-text text-transparent">
+                        Development Stack
+                    </span>
+                </h1>
                      
-            <p>Explore frontend, backend, database, and tooling options, <br/> compare them side by side, and put together the stack that fits your <br/> next project.
+                <p className="text-gray-600">
+                    Explore frontend, backend, database, and tooling options, compare them side by side, and put together the stack that fits your next project.
+                </p>
 
-            </p>
-            <div className="mt-12">
-                 <button className="px-4 py-[8.5px] text-white rounded-md bg-linear-to-r from-[#F97316] to-[#EC4899]">Explore Technologies</button>
+                <div className="mt-8 flex flex-col sm:flex-row justify-center md:justify-start gap-4">
+                    <button className="px-4 py-[8.5px] text-white rounded-md bg-linear-to-r from-[#F97316] to-[#EC4899]">
+                        Explore Technologies
+                    </button>
                 
-                  <button className="btn btn-outline ml-5 -mt-1 border-gray-300 px-12 rounded-md py-4">Learn More</button>
-
-                   
-            </div>
+                    <button className="btn btn-outline border-gray-300 px-12 rounded-md py-4">
+                        Learn More
+                    </button>
+                </div>
               
             </div>
        
-            <img src={Logo} alt="" />
+            <div className="w-full md:w-1/2 flex justify-center">
+                <img src={Logo} alt="Development Stack" className="w-3/4 md:w-full max-w-md object-contain" />
+            </div>
          </div>
-        
-
     );
 };
 
