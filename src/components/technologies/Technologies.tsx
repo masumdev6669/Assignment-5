307727fc97cd991e184998c68d537eed89fae0af
Masum <mahbubmasum2019@gmail.com>
2026-09-13 21:28:33 +0600

    Project finished

--- src/components/technologies/Technologies.tsx
+++ src/components/technologies/Technologies.tsx
@@ -1,4 +1,4 @@
-import React, { use } from 'react';
+import { use } from 'react';
 import type { ITechnologies } from '../../types/technologies';
 import AvailableTechnologies from './AvailableTechnologies';
 
