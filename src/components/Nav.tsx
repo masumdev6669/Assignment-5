054f3e86891f81bb9c35428cde7f5c0ef9dd4626
Masum <mahbubmasum2019@gmail.com>
2026-09-13 20:35:18 +0600

    Responsive Navbar section

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,29 +1,32 @@
-import Logo from "../assets/logo-text.png"
-
+import Logo from "../assets/logo-text.png";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Nav = () => {
-    return (
-         <nav className=" flex justify-between mt-5 container mx-auto">
-               <img src={Logo} alt="" />
-               <ul className="flex gap-5">
-                <li className="text-[#DB2777]"><a href="">Home</a></li>
-                <li><a href="">Technologies</a></li>
-                <li><a href="">Projects</a></li>
-                <li><a href="">About</a></li>
-                <li><a href="">Contact</a></li>
-                
-                
-               </ul>
+  return (
+    <nav className="flex justify-between items-center mt-5 container mx-auto px-4">
+      <img src={Logo} alt="Dev Stack Logo" className="w-32" />
 
-               <div className="flex gap-4 items-center -mt-4">
-                   <button >Sign in</button>
-                   <button className="btn btn-secondary rounded-2xl text-white">Sign Up</button>
-                   
-               </div>
-        
+      <ul className="hidden md:flex gap-5">
+        <li className="text-[#DB2777]"><a href="">Home</a></li>
+        <li><a href="">Technologies</a></li>
+        <li><a href="">Projects</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
 
-      </nav> 
-    );
+      {/* Desktop Buttons (Hidden on mobile) */}
+      <div className="hidden md:flex gap-4 items-center">
+        <button>Sign in</button>
+        <button className="btn btn-secondary rounded-2xl text-white">Sign Up</button>
+      </div>
+
+      {/* Mobile Menu Button (Hidden on desktop) */}
+      <button className="md:hidden text-2xl text-gray-700">
+        <FontAwesomeIcon icon={faBars} />
+      </button>
+    </nav>
+  );
 };
 
 export default Nav;
